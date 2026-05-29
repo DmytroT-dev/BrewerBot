db798c8e0e8e094e52fc1efa9b2285e2b331625e
DmytroT <dmitriytverdokhlibov@yahoo.com>
2026-05-29 12:51:01 +0200

    Add startup log with monitored GitHub username

--- src/main/java/com/brewerbot/BrewerBotApplication.java
+++ src/main/java/com/brewerbot/BrewerBotApplication.java
@@ -1,6 +1,7 @@
 package com.brewerbot;
 
 import com.brewerbot.config.AppProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,8 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties(AppProperties.class)
+@Slf4j
 public class BrewerBotApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BrewerBotApplication.class, args);
+        var ctx = SpringApplication.run(BrewerBotApplication.class, args);
+        var props = ctx.getBean(AppProperties.class);
+        log.info("BrewerBot started. Monitoring GitHub user: {}", props.getGithub().getUsername());
     }
 }
